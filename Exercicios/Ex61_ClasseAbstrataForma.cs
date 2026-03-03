dd727472c5692de53cf5e44df4f9f35f7d582ed5
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2026-03-03 09:40:36 -0300

    Adicionar classe abstrata Forma com método para calcular área e implementação de exemplo.

---
+++ Exercicios/Ex61_ClasseAbstrataForma.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Exercicios;
+
+public abstract class Forma
+{
+	public abstract double CalcularArea();
+}
+
+public class Ex61_ClasseAbstrataForma
+{
+	public static void Executar()
+	{
+		Console.WriteLine("Classe Forma criada como abstrata.");
+	}
+}
