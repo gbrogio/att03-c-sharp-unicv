a2b729db2f22119de9ec54ce7e5993aa1922bf1f
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-03-05 11:18:11 -0300

    Correção da classe

--- Exercicios/Ex65_Polimorfismo.cs
+++ Exercicios/Ex65_Polimorfismo.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 public abstract class Ex65_Polimorfismo
 {
 	public abstract double CalcularArea();
 }
 
-public class  Ex65_Circulo : Ex65_Forma
+public class  Ex65_Circulo : Ex65_Polimorfismo
 {
-    `public double Raio { get; set; }
+    public double Raio { get; set; }
 
     public Ex65_Circulo(double raio)
     {
@@ -24,10 +25,10 @@ public class Ex65_Teste
 {
     public void Executar()
     {
-        List<Ex65_Polimorfismo> formas = new List<Ex65_Polimorfismo>
+        List<Ex65_Circulo> formas = new List<Ex65_Circulo>
         {
             new Ex65_Circulo(2),
             new Ex65_Circulo(5)
-        }
+        };
     }
 }
